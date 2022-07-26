e22e03a471b35dd8b7dfb079eb5e4aa47df946a7
Christian <christian@smdtechnologies.com>
2022-07-26 10:59:55 +0200

    Router fix 1059

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InfoService } from '../services/info/info.service';
 import { NavbarService } from '../services/navbar/navbar.service';
 import { SidenavService } from '../services/sidenav/sidenav.service';
@@ -11,18 +12,26 @@ import { TreeService } from '../services/tree/tree.service';
 })
 export class HomeComponent implements OnInit {
 
+  loggedIn: boolean = false;
+
   constructor(
     public sideNav: SidenavService,
     public topNav: NavbarService,
     public treeNav: TreeService,
-    public info: InfoService
-    ) { }
+    public info: InfoService, 
+    private router: Router
+    ) {
+      this.info.isUserLoggedIn.subscribe(value => {
+        this.loggedIn = value;
+      });
+    }
 
   ngOnInit(): void {
-    this.info.auth();
-    this.topNav.show();
-    this.sideNav.show();
-    this.treeNav.hide();
+    this.router.navigate(['/products']);
+    // this.info.auth();
+    // this.topNav.show();
+    // this.sideNav.show();
+    // this.treeNav.hide();
   }
 
 }
